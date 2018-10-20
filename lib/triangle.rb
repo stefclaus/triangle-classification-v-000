f5a87aa4d7de2c31cdc84a0c215e4944d461b4f9
Stef Claus <stef.claus@gmail.com>
2018-10-20 18:23:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,14 +10,14 @@ class Triangle
    
   end 
 
-  def valid?(triangle_sides)
-    if triangle_sides[0] + triangle_sides[1] < triangle_sides[2]
+  def valid?
+    if @triangle_sides[0] + @triangle_sides[1] < @triangle_sides[2]
       false 
-    elsif triangle_sides[0] <= 0 
+    elsif @triangle_sides[0] <= 0 
       false 
-    elsif triangle_sides[1] <= 0
+    elsif @triangle_sides[1] <= 0
       false 
-    elsif triangle_sides[2] <= 0
+    elsif @triangle_sides[2] <= 0
       false  
     else 
       true  
